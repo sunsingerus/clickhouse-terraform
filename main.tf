b4941a8f9f2efa2e7c5e891c0d7b837e34b8ab1c
Vladislav Klimenko <sunsingerus@gmail.com>
2018-05-22 09:41:01 +0000

    add outputs

--- main.tf
+++ main.tf
@@ -193,18 +193,18 @@ resource "aws_instance" "my_server_3" {
 #
 #data "aws_availability_zones" "all" {}
 #
-#output "ip_server_1" {
-#  value = "${aws_instance.my_server_1.public_ip}"
-#}
-#
-#output "ip_server_2" {
-#  value = "${aws_instance.my_server_2.public_ip}"
-#}
-#
-#output "ip_server_3" {
-#  value = "${aws_instance.my_server_3.public_ip}"
-#}
-#
+output "ip_server_1" {
+  value = "${aws_instance.my_server_1.public_ip}"
+}
+
+output "ip_server_2" {
+  value = "${aws_instance.my_server_2.public_ip}"
+}
+
+output "ip_server_3" {
+  value = "${aws_instance.my_server_3.public_ip}"
+}
+
 #output "elb_dns_name" {
 #  value = "${aws_elb.my_elb_1.dns_name}"
 #}
