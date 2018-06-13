4c7d68dc50de7d2922feaf6ea5f062bf0dc2da5f
Vladislav Klimenko <sunsingerus@gmail.com>
2018-06-13 14:26:42 +0300

    prepare for multiplication

--- clickhouse_external_volume/node_1.tf
+++ clickhouse_external_volume/node_1.tf
@@ -48,6 +48,19 @@ resource "aws_volume_attachment" "ch_ebs_volume_att_1" {
   instance_id = "${aws_instance.ch_node_1.id}"
 }
 
+output "ip_ch_node_1" {
+  value = "${aws_instance.ch_node_1.public_ip}"
+}
+
+output "id_ch_ebs_volume_1" {
+  value = "${aws_ebs_volume.ch_ebs_volume_1.id}"
+}
+
+output "arn_ch_ebs_volume_1" {
+  value = "${aws_ebs_volume.ch_ebs_volume_1.arn}"
+}
+
+
 #
 #
 #
