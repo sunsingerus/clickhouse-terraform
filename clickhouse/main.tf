6eff7f2b24f5b7391a6ae57d47f45318438e43d7
Vladislav Klimenko <sunsingerus@gmail.com>
2018-06-13 11:42:40 +0300

    switch to var in SSH port

--- clickhouse/main.tf
+++ clickhouse/main.tf
@@ -14,8 +14,8 @@ resource "aws_security_group" "ch_node_security_group" {
   vpc_id = "${var.vpc_id}"
 
   ingress {
-    from_port = "22"
-    to_port = "22"
+    from_port = "${var.ssh_port}"
+    to_port = "${var.ssh_port}"
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
