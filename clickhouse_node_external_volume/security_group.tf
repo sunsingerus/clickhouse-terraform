2f23c1952b18bf0be4b6a5c699404959f151cce4
Vladislav Klimenko <sunsingerus@gmail.com>
2018-06-18 14:40:37 +0300

    allow outgoing commections

--- clickhouse_node_external_volume/security_group.tf
+++ clickhouse_node_external_volume/security_group.tf
@@ -32,6 +32,21 @@ resource "aws_security_group" "ch_node_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port = "0"
+    to_port = "65535"
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = "0"
+    to_port = "65535"
+    protocol = "udp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
   lifecycle {
     create_before_destroy = true
   }
